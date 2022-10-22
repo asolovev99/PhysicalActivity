8939c0bd971cd0af5c88960e3ef987b0fc4ca077
SolovevAA <solovevaa@DOM201.LOCAL>
2022-10-22 09:55:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/physicalactivity/ListOfExercises.java
@@ -0,0 +1,15 @@
+package com.example.physicalactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.trusted.ScreenOrientation;
+
+import android.os.Bundle;
+
+public class ListOfExercises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_exercises);
+    }
+}
