b2a609e5e8de7cae6a7ae2b28f6ff67bc9388add
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2022-12-08 10:29:50 +0700

    Add something for sort

--- app/src/main/java/com/example/physicalactivity/ListOfExercises.java
+++ app/src/main/java/com/example/physicalactivity/ListOfExercises.java
@@ -1,15 +1,50 @@
 package com.example.physicalactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.browser.trusted.ScreenOrientation;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ListOfExercises extends AppCompatActivity {
+
+    String[] sorts = { "Название во возрастанию", "Название во убыванию"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_exercises);
+
+        Spinner spinner = findViewById(R.id.spinnerSort);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sorts);
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        // Получаем выбранный объект
+        String item = (String)parent.getItemAtPosition(position);
+        //selection.setText(item);
+        }
+
+                @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 }
